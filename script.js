5682fa7d787d619bd90c1550b83cf360a01cb52f
esgoldman <56517672+esgoldman@users.noreply.github.com>
2024-04-17 21:17:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -31,11 +31,14 @@ function load() {
   Chart.defaults.font.family = "Source Code Pro";
   make_date_chart();
   make_attack_chart();
+  display_data();
 }
+
+var backgroundColor = ["#89CE94", "#FFCB77", "#FFA686", "#E76C6B", "#9CBEB4", "#B1E5CF"];
 
 function make_date_chart() {
   var config = {
-    type: "scatter",
+    type: "bar",
     data: {
       datasets: [],
       labels: [],
@@ -55,6 +58,7 @@ function make_date_chart() {
           },
         },
       },
+      plugins: {},
     },
   };
 
@@ -80,13 +84,15 @@ function make_date_chart() {
           data_map[attackType][date]++;
         });
       });
-
+      var i = 0;
       for (var attackType in data_map) {
         var dataset = {
           label: attackType,
           data: [],
           fill: false,
+          backgroundColor: backgroundColor[i]
         };
+        i++;
         Object.keys(data_map[attackType]).forEach(function (date) {
           dataset.data.push({
             x: date,
@@ -131,7 +137,7 @@ function make_attack_chart() {
         type: "doughnut",
         labels: Object.keys(attacks),
         data: {
-          datasets: [{ data: Object.values(attacks) }],
+          datasets: [{ data: Object.values(attacks), borderWidth: 0, backgroundColor: backgroundColor }],
           labels: Object.keys(attacks),
         },
         options: {},
@@ -149,3 +155,64 @@ function make_attack_chart() {
     },
   });
 }
+
+function display_data() {
+  $.ajax({
+    url: "output.json",
+    dataType: "json",
+    success: function (data) {
+      for (var i = 0; i < data.length; i++) {
+        var entry = data[i];
+        var date = entry.date;
+        var description = entry.description;
+        var attack_type = entry.attack_type;
+        var attack_string = "";
+        if(attack_type.length!=0){
+          attack_string = "- "
+        }
+        for (var j = 0; j < attack_type.length; j++) {
+          attack_string += attack_type[j];
+          if (j != attack_type.length - 1) {
+            attack_string += ", ";
+          }
+        }
+        var damage_type = entry.damage_type;
+        $(".attack-data").append(
+          "<div class='accordion-item'>" +
+            "<h2 class='accordion-header'>" +
+            "<button class='accordion-button' type='button' data-bs-toggle='collapse' data-bs-target='#" +
+            "collapse-" +
+            i +
+            "' aria-expanded='false' aria-controls='" +
+            "collapse-" +
+            i +
+            "'>" +
+            date +
+            " " +
+            attack_string +
+            "</button>" +
+            "</h2>" +
+            "<div class='accordion-collapse collapse' id='" +
+            "collapse-" +
+            i +
+            "' data-bs-parent='.attack-data'>" +
+            "<div class='accordion-body'>" +
+            description +
+            "</div>" +
+            "</div>" +
+            "</div>"
+        );
+      }
+    },
+    error: function (xhr, status, error) {
+      console.error(status, error);
+    },
+  });
+}
+
+$("#search").on("keyup", function() {
+  var value = $(this).val().toLowerCase();
+  $(".accordion-item").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+  });
+});
